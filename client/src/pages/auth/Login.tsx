84dc2b21ad290792cce79b9c4473d7e53633c9fa
Zineb ELGUIL <devzinochan@gmail.com>
2021-12-28 11:54:09 +0000

    added loading and clear error

--- client/src/pages/auth/Login.tsx
+++ client/src/pages/auth/Login.tsx
@@ -1,6 +1,6 @@
 import { EyeOutlined, EyeInvisibleOutlined } from '@ant-design/icons';
 import Button from 'components/UI/Button';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { loginSchema } from 'utils/formValidation';
@@ -11,6 +11,8 @@ import { loginStart } from 'app/slices/authSlice';
 import { ILoginCredentiels } from 'types/types';
 import UseAuth from './UseAuth';
 import { useAppSelector } from 'app/store';
+import { loadingAuth } from 'app/slices/loadingSlice';
+import { authError } from 'app/slices/errorSlice';
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -30,6 +32,11 @@ const Login = () => {
   const onSubmit = (data: ILoginCredentiels) => {
     dispatch(loginStart(data));
   };
+
+  useEffect(() => {
+    dispatch(loadingAuth(false));
+    dispatch(authError(null));
+  }, [dispatch]);
 
   return (
     <UseAuth>
