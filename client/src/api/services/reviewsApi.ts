5c82077094a0afb29122afd9507f8c75c78c2bfd
Zineb ELGUIL <devzinochan@gmail.com>
2022-06-05 21:46:25 +0100

    add: reviews api

---
+++ client/src/api/services/reviewsApi.ts
@@ -0,0 +1,10 @@
+import { IReview } from 'types/types';
+import httpRequest from '../axios';
+
+export const addReview = (slug: string | undefined, review: IReview, token: string) =>
+  httpRequest({
+    method: 'POST',
+    url: `/reviews/${slug}`,
+    headers: { authorization: `Bearer ${token}` },
+    data: review,
+  });
