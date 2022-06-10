1e55c548d51866f2140dc3e9c9de6c47300be73b
Zineb ELGUIL <devzinochan@gmail.com>
2022-06-10 12:35:19 +0100

    add: del product btn & fix category field

--- client/src/pages/admin/AllProducts.tsx
+++ client/src/pages/admin/AllProducts.tsx
@@ -1,14 +1,16 @@
 import { useAppSelector } from 'app/store';
 import { useEffect } from 'react';
-import { getProductsStart } from 'app/slices/productSlice';
+import { delProductStart, getProductsStart } from 'app/slices/productSlice';
 import { useDispatch } from 'react-redux';
 import Loading from 'components/loaders/Loading';
+import { FaTrashAlt } from 'react-icons/fa';
 
 const AllProducts = () => {
-  const { products, isLoadingProducts, isErrorProducts } = useAppSelector((state) => ({
+  const { products, isLoadingProducts, isErrorProducts, token } = useAppSelector((state) => ({
     products: state.products,
     isLoadingProducts: state.loadingState.isLoadingProducts,
     isErrorProducts: state.errorState.isErrorProducts,
+    token: state.auth.token,
   }));
   const dispatch = useDispatch();
 
@@ -18,29 +20,38 @@ const AllProducts = () => {
     }
   }, [dispatch, products.length]);
 
+  const onProductDelete = (id: string) => {
+    dispatch(delProductStart({ id, token }));
+  };
+
   return (
-    <section className="py-6 bg-white mt-6">
+    <section className="py-6  md:px-8 px-2 bg-white mt-6">
       {isLoadingProducts && <Loading />}
       {!isErrorProducts && products.length > 0 && (
-        <div className="relative overflow-x-auto shadow-md sm:rounded-lg md:px-8 px-2">
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
           <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
               <tr className="capitalize">
-                <th scope="col" className="px-6 py-3"></th>
-                <th scope="col" className="px-6 py-3">
+                <th scope="col" className="px-2 py-3">
+                  Image
+                </th>
+                <th scope="col" className="px-2 py-3">
                   PRODUCT NAME
                 </th>
-                <th scope="col" className="px-6 py-3">
+                <th scope="col" className="px-2 py-3">
                   CATEGORY
                 </th>
-                <th scope="col" className="px-6 py-3">
+                <th scope="col" className="px-2 py-3">
                   Stock count
                 </th>
-                <th scope="col" className="px-6 py-3">
+                <th scope="col" className="px-2 py-3">
                   Sold count
                 </th>
-                <th scope="col" className="px-6 py-3">
-                  <span className="sr-only">rating</span>
+                <th scope="col" className="px-2 py-3">
+                  rating
+                </th>
+                <th scope="col" className="px-2 py-3">
+                  action
                 </th>
               </tr>
             </thead>
@@ -51,11 +62,16 @@ const AllProducts = () => {
                     <th className="p-4">
                       <img className="w-16 h-16 rounded" src={product.image} alt={product.title} />
                     </th>
-                    <td className="px-6 py-4 font-medium text-gray-900  whitespace-nowrap">{product.title}</td>
-                    <td className="px-6 py-4">{product.category}</td>
-                    <td className="px-6 py-4">{product.countInStock}</td>
-                    <td className="px-6 py-4">{product.sold}</td>
-                    <td className="px-6 py-4">{product.rating}</td>
+                    <td className="px-2 py-3 font-medium text-gray-900  whitespace-nowrap">{product.title}</td>
+                    <td className="px-2 py-3">{product.category?.name}</td>
+                    <td className="px-2 py-3">{product.countInStock}</td>
+                    <td className="px-2 py-3">{product.sold}</td>
+                    <td className="px-2 py-3">{product.rating}</td>
+                    <td className="px-2 py-3">
+                      <span onClick={() => onProductDelete(product._id)} className="text-red-600">
+                        <FaTrashAlt />
+                      </span>
+                    </td>
                   </tr>
                 ))}
             </tbody>
