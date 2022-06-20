5555e84074f2dea0e10b81ecfbe5bbf9a4a1da12
Zineb ELGUIL <devzinochan@gmail.com>
2022-06-20 21:39:16 +0100

    refact: use wishlist from state

--- client/src/pages/profile/Wishlist.tsx
+++ client/src/pages/profile/Wishlist.tsx
@@ -1,20 +1,25 @@
+import { useAppSelector } from 'app/store';
 import WishItem from './components/WishItem';
 
 const Wishlist = () => {
+  const wishlist = useAppSelector((state) => state.wishlist);
+
   return (
     <div className="py-6">
       <div className="px-2">
         <h3 className="font-semibold text-lg text-primaryDark capitalize  mb-4 pb-2 border-b-2 border-gray-50">
           Your wishlist
         </h3>
-
-        <div className="max-w-screen-sm mx-auto">
-          {new Array(2).fill({}).map((product, index) => (
-            <div key={`product-${index}`} className="mb-4">
-              <WishItem />
-            </div>
-          ))}
-        </div>
+        {wishlist.length === 0 && <h3 className="text-center">Your wishlist is empty</h3>}
+        {wishlist.length > 0 && (
+          <div className="max-w-screen-sm mx-auto">
+            {wishlist.map((wishItem) => (
+              <div key={wishItem.productId} className="mb-4">
+                <WishItem wishItem={wishItem} />
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
--- client/src/pages/profile/components/WishItem.tsx
+++ client/src/pages/profile/components/WishItem.tsx
@@ -1,17 +1,30 @@
 import { DeleteOutlined } from '@ant-design/icons';
+import { ICart } from 'app/slices/cartSlice';
 import Button from 'components/UI/Button';
+import useCart from 'utils/hooks/useCart';
 
-const WishItem = () => {
+type Props = {
+  wishItem: ICart;
+};
+
+const WishItem = ({ wishItem }: Props) => {
+  const { onAddToCart } = useCart();
   return (
     <div className="grid lg:grid-cols-7 grid-cols-5 shadow-md bg-gray-50 items-center gap-4 p-4">
-      <div className=" w-full bg-gray-200 col-span-2 rounded-md h-32 bg-center bg-contain bg-no-repeat"></div>
+      <img
+        src={wishItem.image}
+        alt={wishItem.title}
+        className=" w-full bg-gray-200 col-span-2 rounded-md h-32 bg-center bg-contain bg-no-repeat"
+      />
       <div className="col-span-3">
-        <h3 className=" text-sm col-span-3 mb-4">product</h3>
-        <h4 className="text-md">$ 12</h4>
+        <h3 className=" text-sm col-span-3 mb-4">{wishItem.title}</h3>
+        <h4 className="text-md">$ {wishItem.price}</h4>
       </div>
 
       <div className="flex lg:flex-col  lg:col-span-2 col-span-5 h-full items-center justify-between">
-        <Button theme="btn-dark">add to cart</Button>
+        <Button onClick={() => onAddToCart(wishItem)} theme="btn-dark">
+          add to cart
+        </Button>
         <span className="text-red-500 cursor-pointer text-lg">
           <DeleteOutlined />
         </span>
