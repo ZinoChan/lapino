d3596d074e965fa8dceaf7875bf38be660bbcaa7
ZinoChan <devzinochan@gmail.com>
2022-08-17 22:29:56 +0100

    refactor: show list of product variants

--- client/src/pages/product/components/CartBtn.tsx
+++ client/src/pages/product/components/CartBtn.tsx
@@ -1,15 +1,17 @@
 import Button from '@/components/UI/Button';
-import { IProductRes } from '@/types/types';
+import { IProductRes, IVariant } from '@/types/types';
 import { formCartItem } from '@/utils/helpers';
 import useCart from '@/utils/hooks/useCart';
-import { Confirm } from 'react-st-modal';
+import { StaticDialog } from 'react-st-modal';
 import { ColorCircle } from './Colors';
 import { Size } from './Sizes';
+import { ICart } from '@/app/slices/cartSlice';
+import { useEffect, useState } from 'react';
 
 type ModelProps = {
-  handleAddToCart: () => void;
-  size: string | null;
-  color: string | null;
+  onIncrementVariant: (id: string, variantKey: string) => void;
+  findItem: (id: string) => ICart | undefined;
+  id: string;
 };
 
 type CartBtnProps = {
@@ -19,24 +21,23 @@ type CartBtnProps = {
 };
 
 const CartBtn = ({ product, selectedSize, selectedColor }: CartBtnProps) => {
-  const { onAddToCart, isItemInCart, onAddQty, onMinusQty, findItem } = useCart();
+  const { onAddToCart, isItemInCart, onAddQty, onMinusQty, findItem, onIncrementVariant } = useCart();
   const { _id } = product;
+  const foundItem = findItem(_id);
 
   const handleAddToCart = () => {
-    const cartItem = formCartItem(product);
+    const cartItem = formCartItem(product, selectedSize, selectedColor);
     onAddToCart(cartItem);
   };
 
   return (
     <>
       {!isItemInCart(_id) ? (
-        product.size.length === 0 && product.color.length === 0 ? (
-          <Button onClick={handleAddToCart} theme="btn-large md:w-auto w-full">
-            Add to cart
-          </Button>
-        ) : (
-          <ConfirmVariants size={selectedSize} color={selectedColor} handleAddToCart={handleAddToCart} />
-        )
+        <Button onClick={handleAddToCart} theme="btn-large md:w-auto w-full">
+          Add to cart
+        </Button>
+      ) : foundItem?.variants ? (
+        <ConfirmVariants findItem={findItem} onIncrementVariant={onIncrementVariant} id={_id} />
       ) : (
         <div className="flex items-center space-x-4">
           <Button onClick={() => onAddQty(_id)} theme="btn-gray self-end">
@@ -52,41 +53,56 @@ const CartBtn = ({ product, selectedSize, selectedColor }: CartBtnProps) => {
   );
 };
 
-function ConfirmVariants({ size, color, handleAddToCart }: ModelProps) {
+function ConfirmVariants({ findItem, id, onIncrementVariant }: ModelProps) {
+  const foundItem = findItem(id);
+  const [isOpen, setOpen] = useState(false);
   return (
     <div>
-      <Button
-        theme="btn-large md:w-auto w-full"
-        onClick={async () => {
-          const result = await Confirm(
-            <div className="p-4">
-              {size && (
-                <span className="flex justify-between mb-2">
-                  <span className="text-gray-500 text-xl">Selected Size: </span>
-                  <div className="flex items-center space-x-2">
-                    <Size size={size} selectedSize />
-                    <span className="text-sm">Months</span>
-                  </div>
-                </span>
-              )}
-              {color && (
-                <span className="flex justify-between mb-2">
-                  <span className="text-gray-500 text-xl">Selected Color: </span>
-                  <div className="flex items-center space-x-2">
-                    <ColorCircle color={color} selectedColor />
-                  </div>
-                </span>
-              )}
-            </div>,
-          );
-
-          if (result) {
-            handleAddToCart();
-          }
+      <StaticDialog
+        isOpen={isOpen}
+        title="Custom static dialog"
+        onAfterClose={(result: any) => {
+          setOpen(false);
         }}
       >
-        Add to cart
-      </Button>
+        <div className="p-4">
+          {foundItem?.variants &&
+            foundItem.variants.map((variant) => (
+              <div key={variant.key} className="flex justify-between items-center">
+                {variant.size && (
+                  <span className="flex items-center space-x-2">
+                    <span> size:</span> <Size size={variant.size} selectedSize /> <span>Months</span>
+                  </span>
+                )}
+                {variant.color && (
+                  <span className="flex items-center space-x-2">
+                    <span> color: </span> <ColorCircle color={variant.color} selectedColor />
+                  </span>
+                )}
+                <div className="flex items-center space-x-2">
+                  <button
+                    onClick={() => onIncrementVariant(foundItem.productId, variant.key)}
+                    className="btn-small rounded bg-primary text-white"
+                  >
+                    +
+                  </button>
+                  <span>{variant.qty}</span>
+                  <button className="btn-small rounded bg-primary text-white">-</button>
+                </div>
+              </div>
+            ))}
+        </div>
+      </StaticDialog>
+      <button
+        className="flex items-center space-x-4"
+        onClick={() => {
+          setOpen(true);
+        }}
+      >
+        <span className="btn btn-gray">+</span>
+        <span>{foundItem?.qty}</span>
+        <span className="btn btn-gray">-</span>
+      </button>
     </div>
   );
 }
