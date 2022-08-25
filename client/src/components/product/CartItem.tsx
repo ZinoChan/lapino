d86b9f45265672648b08d70658f67f1288499746
ZinoChan <devzinochan@gmail.com>
2022-08-25 16:43:16 +0100

    feat: add variant modal

--- client/src/components/product/CartItem.tsx
+++ client/src/components/product/CartItem.tsx
@@ -2,13 +2,14 @@ import { DeleteFilled } from '@ant-design/icons';
 import { ICart } from '@/app/slices/cartSlice';
 import Button from '@/components/UI/Button';
 import useCart from '@/utils/hooks/useCart';
+import VariantsModel from '@/pages/product/components/VariantsModel';
 
 type CartItemProps = {
   cartItem: ICart;
 };
 
 const CartItem = ({ cartItem }: CartItemProps) => {
-  const { onMinusQty, onAddQty, onRemoveFromCart } = useCart();
+  const { onMinusQty, onAddQty, onRemoveFromCart, findItem, onDecrementVariant, onIncrementVariant } = useCart();
   const id = cartItem.productId;
   return (
     <div className="bg-white rounded-md shadow-md p-4 mb-4">
@@ -30,15 +31,24 @@ const CartItem = ({ cartItem }: CartItemProps) => {
           <div className="text-right cursor-pointer" onClick={() => onRemoveFromCart(id)}>
             <DeleteFilled className="text-red-500 text-2xl" />
           </div>
-          <div className="flex items-center space-x-4">
-            <Button onClick={() => onAddQty(id)} theme="btn-gray lg:self-end">
-              +
-            </Button>
-            <span className="font-bold">{cartItem.qty}</span>
-            <Button onClick={() => onMinusQty(id)} theme="btn-gray">
-              -
-            </Button>
-          </div>
+          {cartItem.variants ? (
+            <VariantsModel
+              id={id}
+              findItem={findItem}
+              onDecrementVariant={onDecrementVariant}
+              onIncrementVariant={onIncrementVariant}
+            />
+          ) : (
+            <div className="flex items-center space-x-4">
+              <Button onClick={() => onAddQty(id)} theme="btn-gray lg:self-end">
+                +
+              </Button>
+              <span className="font-bold">{cartItem.qty}</span>
+              <Button onClick={() => onMinusQty(id)} theme="btn-gray">
+                -
+              </Button>
+            </div>
+          )}
         </div>
       </div>
     </div>
