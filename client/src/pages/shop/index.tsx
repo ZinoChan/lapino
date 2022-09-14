dcc4f434294af774adc0774822b8489bcfd8f93f
ZinoChan <devzinochan@gmail..com>
2022-09-14 22:09:08 +0100

    feat: add shop filtering

--- client/src/pages/shop/index.tsx
+++ client/src/pages/shop/index.tsx
@@ -2,19 +2,26 @@ import PriceRange from './components/PriceRange';
 import DiscountRange from './components/DiscountRange';
 import RatingRange from './components/RatingRange';
 import ProductList from '@/components/product/ProductList';
-import { useAppSelector } from '@/app/store';
+import {useLocation} from 'react-router-dom'
+import { useSearch } from '@/utils/hooks/useSearch';
+import Loading from '@/components/loaders/Loading';
+
 
 const Shop = () => {
-  const products = useAppSelector((state) => state.products);
+  const location = useLocation() 
+  const {products, isLoading, isError} = useSearch(`${location.search}`)
+ 
+
   return (
     <section className="py-10">
+      {isLoading && <Loading/>}
       <div className="grid grid-cols-5 gap-2">
         <div className=" border border-gray-200 py-6  bg-white shadow px-4">
-          <PriceRange />
-          <DiscountRange />
-          <RatingRange />
+          <PriceRange  searchQuery={location.search}/>
+          <DiscountRange searchQuery={location.search} />
+          <RatingRange searchQuery={location.search} />
         </div>
-        {products.length > 0 && <ProductList products={products} hasBtn />}
+        {products?.length > 0 && products.length > 0 && <ProductList products={products} hasBtn />}
       </div>
     </section>
   );
