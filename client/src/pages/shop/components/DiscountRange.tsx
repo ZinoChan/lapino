e00a7d356887c3a44e72f44228bb6c9fa57b13b1
ZinoChan <devzinochan@gmail..com>
2022-09-14 22:10:04 +0100

    feat: add discount filtering

--- client/src/pages/shop/components/DiscountRange.tsx
+++ client/src/pages/shop/components/DiscountRange.tsx
@@ -1,13 +1,30 @@
-const DiscountRange = () => {
-  const discountRanges: number[] = [10, 20, 30, 40, 50];
+import { SHOP } from "@/utils/routes";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
+
+
+const DiscountRange = ({searchQuery}: {searchQuery: string}) => {
+  const discountRanges: number[] = [10, 20, 30, 40, 50];
+  const [selectedDiscount, setSelectedDiscount] = useState(0)
+  const navigate = useNavigate()
+  
+  const onDiscountChange = (range: number) => {
+      setSelectedDiscount(range)
+      const discountFilter = `pricing.discountPercentage[gte]=${range}`
+      if(searchQuery){
+        navigate(`${SHOP}${searchQuery}&&${discountFilter}`)
+      }else{
+        navigate(`${SHOP}?${discountFilter}`)
+      }
+  }
   return (
     <div className="mb-10">
       <h3 className="font-semibold text-md uppercase mb-4">Discount percentage</h3>
       <div className="flex flex-col space-y-4">
         {discountRanges.map((range) => (
           <div key={range} className="flex items-center space-x-4">
-            <input type="radio" className="w-4 h-4" />
+            <input value={range} onChange={() => onDiscountChange(range)} checked={selectedDiscount === range} type="radio" className="w-4 h-4" />
             <label className="text-md font-semibold">
               <span className="mx-2">{`>`}</span>
               {range}%
