19fc490b4ddec34f1f87ce9cd7d90efcc6cf4401
ZinoChan <devzinochan@gmail..com>
2022-09-14 22:10:32 +0100

    feat: rating filter

--- client/src/pages/shop/components/RatingRange.tsx
+++ client/src/pages/shop/components/RatingRange.tsx
@@ -1,7 +1,23 @@
 import { RatingView } from 'react-simple-star-rating';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { SHOP } from '@/utils/routes';
 
-const RatingRange = () => {
+
+const RatingRange = ({searchQuery}: {searchQuery: string}) => {
   const ratingValues = [4, 3, 2, 1];
+  const [selectedRate, setSelectedRate] = useState(0)
+  const navigate = useNavigate()
+  
+  const onRateChange = (value: number) => {
+      setSelectedRate(value)
+      const ratingFilter = `rating[gte]=${value}`
+      if(searchQuery){
+        navigate(`${SHOP}${searchQuery}&&${ratingFilter}`)
+      }else{
+        navigate(`${SHOP}?${ratingFilter}`)
+      }
+  }
 
   return (
     <div className="mb-10">
@@ -9,7 +25,7 @@ const RatingRange = () => {
       <div className="flex flex-col space-y-4">
         {ratingValues.map((value, index) => (
           <div key={value} className="flex items-center space-x-4">
-            <input type="radio" className="w-4 h-4" />
+            <input value={value} onChange={() => onRateChange(value)} type="radio" checked={selectedRate === value} className="w-4 h-4" />
             <div>
               <RatingView fillColor={'#fed900'} ratingValue={value} />
             </div>
