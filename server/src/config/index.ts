31b142732f11282ff0b360b5878867ce976d91c4
ZinoChan <devzinochan@gmail..com>
2022-09-16 19:06:52 +0100

    minor change

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,4 +1,4 @@
 import { config } from 'dotenv';
 
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
-export const { NODE_ENV, PORT, MONGO_URI, JWT_SECRET, ORIGIN, PROJECT_ID, CLIENT_EMAIL, PRIVATE_KEY } = process.env;
+export const { NODE_ENV, PORT, MONGO_URI, JWT_SECRET, ORIGIN, PROJECT_ID, CLIENT_EMAIL, PRIVATE_KEY, STORAGE_BUCKET } = process.env;
