47aee1d1218f6076ef19609cad95466a74d74cb5
ZinoChan <devzinochan@gmail..com>
2022-09-19 22:11:52 +0100

    remove upload folder

--- server/src/components/upload/uploadRoute.ts
+++
@@ -1,9 +0,0 @@
-import { isAdmin, isAuthenticated } from '@/middlewares/auth.middleware';
-import { Router } from 'express';
-import { uploader, uploadImage } from './uploadController';
-
-const router = Router();
-
-router.route('/').post(isAuthenticated, isAdmin, uploader.single('image'), uploadImage);
-
-export default router;
