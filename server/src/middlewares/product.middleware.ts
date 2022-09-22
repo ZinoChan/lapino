db887aa9daa837efd302c5d5634e995bc2d79fd3
ZinoChan <devzinochan@gmail..com>
2022-09-22 11:53:19 +0100

    fix

--- server/src/middlewares/product.middleware.ts
+++ server/src/middlewares/product.middleware.ts
@@ -1,18 +1,24 @@
-import {Request, NextFunction, Response } from "express";
+import {NextFunction, Response } from "express";
 import { IProduct } from "@/components/products/product";
 import { ErrorHandler } from "./error.middleware";
+import { MulterRequest } from "@/types/types";
 
-const isValidProduct =  async (req: Request, res: Response, next: NextFunction) => {
+const isValidProduct =  async (req: MulterRequest, res: Response, next: NextFunction) => {
+   try {
     const product: IProduct = req.body
-        if (
-          product.title &&
-          product.description &&
-          product.details &&
-          product.pricing.originalPrice &&
-          product.category &&
-          product.countInStock 
-        ) next()
-        next(new ErrorHandler(404, 'missing required product fields'))
+    if (
+      product.title &&
+      product.description &&
+      product.details &&
+      product.pricing.originalPrice &&
+      product.category &&
+      product.countInStock 
+    ) return  next()
+    
+    return next(new ErrorHandler(400, 'missing required product fields'))
+   } catch (err) {
+    return next(err)
+   }
     };
 
 export {isValidProduct}
