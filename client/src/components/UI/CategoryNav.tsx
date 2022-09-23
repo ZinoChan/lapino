5d0639ef681b549bef0ca5ca2fd6e86618752276
ZinoChan <devzinochan@gmail..com>
2022-09-23 22:08:17 +0100

    refactor: filtering category by slug

--- client/src/components/UI/CategoryNav.tsx
+++ client/src/components/UI/CategoryNav.tsx
@@ -8,11 +8,11 @@ interface Props {
 const CategoryNav = ({ categories }: Props) => {
   return (
     <ul className="flex lg:justify-between space-y-4 lg:space-y-0 lg:px-8 px-2 h-full py-2 flex-col ">
-      {categories.map(({ name, image, _id }) => (
+      {categories.map(({ name, image, _id, slug }) => (
         <li key={_id} className="font-semibold text-md transition-all">
           <Link
             className="hover:opacity-70 flex space-x-2 items-center  text-primaryDark"
-            to={`/shop?category=${name}`}
+            to={`/shop?category=${slug}`}
           >
             <span>
               <img src={image} alt="category" className="w-4 h-4" />
--- server/src/helpers/filterAPI.ts
+++ server/src/helpers/filterAPI.ts
@@ -1,17 +1,26 @@
+import category from "@/components/categories/category";
+import { ErrorHandler } from "@/middlewares/error.middleware";
+
 class APIFilters {
     query: any;
-    queryString: {string};
+    queryString: {[key:string]: string};
     constructor(query, queryString) {
         this.query = query
         this.queryString = queryString
     }
 
-    filtreing() {
+    async filtreing() {
         const queryobj = {...this.queryString}
+        if(queryobj.category){
+            const categoryExists = await category.findOne({slug: queryobj.category})
+            if(categoryExists) queryobj.category = categoryExists._id
+            else throw new ErrorHandler(404, 'category not found')
+        }
         const excludedfields = ['page', 'sort', 'limit']
         excludedfields.forEach(el => delete queryobj[el])
         let querystr = JSON.stringify(queryobj)
         querystr = querystr.replace(/\b(gte|gt|lt|lte)\b/g, match => `$${match}`)
+       
         this.query.find(JSON.parse(querystr))
         return this
     }
