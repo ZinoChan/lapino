e5d73e1a04f7f8c0055334030b8789105b73fb02
ZinoChan <devzinochan@gmail..com>
2022-09-23 22:11:26 +0100

    feat: validate category

---
+++ server/src/middlewares/category.middleware.ts
@@ -0,0 +1,24 @@
+import {NextFunction, Response } from "express";
+import { ICategory } from "@/components/categories/category";
+import { ErrorHandler } from "./error.middleware";
+import { MulterRequest } from "@/types/types";
+
+const isValidCategory =  async (req: MulterRequest, res: Response, next: NextFunction) => {
+   try {
+    
+    const category: ICategory = req.body
+    if (category.parent === null && !req.file) {
+        return next(new ErrorHandler(400, 'Category image is needed for main category' ));
+    }
+    if (category.name) {
+        req.file.category = true
+        return next()
+    }
+    
+    return next(new ErrorHandler(400, 'missing category name'))
+   } catch (err) {
+    return next(err)
+   }
+    };
+
+export {isValidCategory}
