568ca272ff82927e491dc76a7f56cb4b657a1529
ZinoChan <devzinochan@gmail..com>
2022-09-23 16:08:12 +0100

    mino change

--- server/src/components/categories/categoryService.ts
+++ server/src/components/categories/categoryService.ts
@@ -1,12 +1,12 @@
 import Category, { ICategory } from './category';
-import slugify from 'slugify';
 import { ErrorHandler } from '@/middlewares/error.middleware';
 
 class CategoryService {
   async addCategory(category: ICategory): Promise<ICategory> {
     try {
       const categoryParentId = category.parent ? category.parent : null;
-      const categoryExists = await Category.findOne({ slug: slugify(category.name) });
+      const {slug} = category
+      const categoryExists = await Category.findOne({ slug });
       if (categoryExists) {
         throw new ErrorHandler(400, 'Category with this name already exists');
       }
