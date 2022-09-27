e71b9a95b3e4f402972f7ecc8a7bb7c73d68c372
ZinoChan <devzinochan@gmail.com>
2022-09-27 22:06:26 +0100

    minor change

--- client/src/pages/product/components/ReviewList.tsx
+++ client/src/pages/product/components/ReviewList.tsx
@@ -1,7 +1,7 @@
 import { IReviewRes } from '@/types/types';
 import Review from './Review';
 import '@/styles/pages/review.css';
-import {FaStar} from 'react-icons/fa'
+import { FaStar } from 'react-icons/fa';
 
 type ReviewsListProps = {
   reviews: IReviewRes[];
@@ -15,12 +15,19 @@ const ReviewList = ({ reviews, rating, numReviews }: ReviewsListProps) => {
       <div className="mb-6 pb-2 border-b border-gray-200  flex justify-between items-center">
         <h3 className="font-main font-bold">Reviews</h3>
         <p className="text-bold flex items-center">
-         <span className="flex space-x-1 items-center mr-3">{rating.toFixed(1)} <FaStar className='text-yellow-400'/></span> 
-             ({numReviews})
+          <span className="flex space-x-1 items-center mr-3">
+            {rating.toFixed(1)} <FaStar className="text-yellow-400" />
+          </span>
+          ({numReviews})
         </p>
       </div>
       <div className="max-h-52 overflow-y-scroll px-4  ">
         {reviews?.length > 0 && reviews.map((review) => <Review key={review._id} review={review} />)}
+        {reviews.length < 1 && (
+          <div className="flex items-center justify-center">
+            <span className="text-gray-500">No Reviews</span>
+          </div>
+        )}
       </div>
     </div>
   );
