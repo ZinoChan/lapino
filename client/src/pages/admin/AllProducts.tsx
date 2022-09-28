1eae9531d80373e95507e63397812439098ac3de
ZinoChan <devzinochan@gmail.com>
2022-09-28 23:27:15 +0100

    fix category name not displayed

--- client/src/pages/admin/AllProducts.tsx
+++ client/src/pages/admin/AllProducts.tsx
@@ -5,12 +5,16 @@ import { useDispatch } from 'react-redux';
 import Loading from '@/components/loaders/Loading';
 import { FaTrashAlt } from 'react-icons/fa';
 
+
+import { getCategoriesStart } from '@/app/slices/categorySlice';
+
 const AllProducts = () => {
-  const { products, isLoadingProducts, isErrorProducts, token } = useAppSelector((state) => ({
+  const { products, isLoadingProducts, isErrorProducts, token, categories } = useAppSelector((state) => ({
     products: state.products,
     isLoadingProducts: state.loadingState.isLoadingProducts,
     isErrorProducts: state.errorState.isErrorProducts,
     token: state.auth.token,
+    categories: state.categories
   }));
   const dispatch = useDispatch();
 
@@ -18,15 +22,25 @@ const AllProducts = () => {
     if (products.length === 0) {
       dispatch(getProductsStart());
     }
-  }, [dispatch, products.length]);
+    if(categories.length ===0 ) {
+      dispatch(getCategoriesStart())
+    }
+  }, [dispatch, products.length, categories.length]);
 
   const onProductDelete = (id: string) => {
     dispatch(delProductStart({ id, token }));
   };
 
+  const findCategory =  (id: string) => categories.find(cat => cat._id === id )
+
   return (
     <section className="py-6  md:px-8 px-2 bg-white mt-6">
       {isLoadingProducts && <Loading />}
+      {!isErrorProducts && !isLoadingProducts && products.length === 0 && <div className="text-center py-6">
+        <h1 className="text-capitalize font-bold text-gray-600">
+          No Product was found
+        </h1>
+      </div> }
       {!isErrorProducts && products.length > 0 && (
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
           <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
@@ -63,7 +77,7 @@ const AllProducts = () => {
                       <img className="w-16 h-16 rounded" src={product.image} alt={product.title} />
                     </th>
                     <td className="px-2 py-3 font-medium text-gray-900  whitespace-nowrap">{product.title}</td>
-                    <td className="px-2 py-3">{product.category?.name}</td>
+                    <td className="px-2 py-3">{findCategory(product.category)?.name}</td>
                     <td className="px-2 py-3">{product.countInStock}</td>
                     <td className="px-2 py-3">{product.sold}</td>
                     <td className="px-2 py-3">{product.rating}</td>
