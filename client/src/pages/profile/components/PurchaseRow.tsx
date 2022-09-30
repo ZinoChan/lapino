8d86c187c1a0741797318f51ac7727aab66f60a7
ZinoChan <devzinochan@gmail.com>
2022-09-30 15:36:01 +0100

    style

--- client/src/pages/profile/components/PurchaseRow.tsx
+++ client/src/pages/profile/components/PurchaseRow.tsx
@@ -11,9 +11,11 @@ const PuchaseRow = ({ orderStatus, total, orderItems, createdAt }: Props) => {
   return (
     <div className="hover:bg-gray-50 py-4 cursor-pointer grid lg:grid-cols-6 grid-cols-1 gap-2 items-center lg:justify-items-center border-b-2 border-gray-100 ">
       <div className="grid lg:grid-cols-4  w-full  lg:col-span-2">
-        <h3 className="font-bold capitalize flex items-center justify-center space-x-4 w-full text-center col-span-4  font-sm ">
-          <span className="text-base text-gray-600 hover:text-primary"></span>
-          <span>{displayTime(createdAt)}</span>
+        <h3 className="font-semibold capitalize flex items-center justify-center space-x-4 w-full text-center col-span-4  font-sm ">
+          <span className="text-sm text-gray-600 hover:text-pgray-700">
+          {displayTime(createdAt)}
+          </span>
+         
         </h3>
       </div>
       <div className="lg:block lg:space-x-0 flex items-center space-x-4">
