48f29fb26d62a1695bed860c0352e44528d68610
ZinoChan <devzinochan@gmail.com>
2022-10-02 17:10:27 +0100

    add disabled state

--- client/src/components/UI/Button.tsx
+++ client/src/components/UI/Button.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-
 interface Props {
   theme: string;
   children: React.ReactNode;
   className?: string;
   onClick?: () => void;
   type?: 'submit' | 'reset' | 'button';
+  disabled?: boolean;
 }
 
-const Button: React.FC<Props> = ({ theme, className = '', children, onClick, type }) => {
+const Button: React.FC<Props> = ({ theme, className = '', children, onClick, type, disabled = false }) => {
   return (
-    <button type={type} onClick={onClick} className={`btn ${theme} ${className}`}>
+    <button disabled={disabled} type={type} onClick={onClick} className={`btn ${theme} ${className}`}>
       {children}
     </button>
   );
