a9b42e8b9edc32724b5d310a06f2fbd92d43cd6c
ZinoChan <devzinochan@gmail.com>
2022-10-05 17:35:12 +0100

    style

--- client/src/components/UI/SearchBar.tsx
+++ client/src/components/UI/SearchBar.tsx
@@ -5,13 +5,12 @@ import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import Button from './Button';
 
-
 type SearchType = {
   searchWord: string;
-}
+};
 
 const SearchBar = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
@@ -25,15 +24,20 @@ const SearchBar = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-4 sm:gap-2 lg:w-max w-full lg:justify-items-end gap-2">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="grid grid-cols-4 sm:gap-2 lg:w-max w-full lg:justify-items-end gap-2"
+    >
       <div className="relative col-span-3 ">
-        <input {...register('searchWord', {required: true, min: 3, max: 100}) } type="text" className="bg-gray-200  appearance-none border-2 border-gray-200 rounded w-full py-1 px-6 text-gray-700 leading-tight focus:outline-none focus:border-primaryDark " />
+        <input
+          {...register('searchWord', { required: true, min: 3, max: 100 })}
+          type="text"
+          className="bg-gray-200  appearance-none border-2 border-gray-200 rounded w-full py-1 px-6 text-gray-700 leading-tight focus:outline-none focus:border-primaryDark "
+        />
         <span className="absolute inline-flex top-1/2 left-0.5 text-2xl text-gray-400 transform -translate-y-1/2">
           <SearchOutlined className="text-gray-500" />
         </span>
-        <span className="text-red-500">
-          {errors.searchWord && errors.searchWord.message}
-        </span>
+        <span className="text-red-500">{errors.searchWord && errors.searchWord.message}</span>
       </div>
       <Button theme="btn-dark">Search</Button>
     </form>
