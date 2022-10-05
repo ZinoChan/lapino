e86b87a0ef28c3fab2adc731d244727240b1d32f
ZinoChan <devzinochan@gmail.com>
2022-10-05 17:36:56 +0100

    stop refetch after error

--- client/src/utils/hooks/useProduct.ts
+++ client/src/utils/hooks/useProduct.ts
@@ -4,7 +4,7 @@ import useSWR from 'swr';
 const fetcher = (url: string) => axios.get(url).then((res) => res.data.data);
 
 export function useProduct(slug: string | undefined) {
-  const { data, error } = useSWR(`/products/${slug}`, fetcher);
+  const { data, error } = useSWR(`/products/${slug}`, fetcher, { shouldRetryOnError: false });
 
   return {
     product: data,
--- client/src/utils/hooks/useSearch.ts
+++ client/src/utils/hooks/useSearch.ts
@@ -4,8 +4,7 @@ import useSWR from 'swr';
 const fetcher = (url: string) => axios.get(url).then((res) => res.data.data);
 
 export function useSearch(searchUrl: string | undefined) {
-  const { data, error } = useSWR(searchUrl, fetcher);
-
+  const { data, error } = useSWR(searchUrl, fetcher, { shouldRetryOnError: false });
 
   return {
     products: data,
