f0a3e2026b2cd3ed52e9edb8a829de9cbc993082
ZinoChan <devzinochan@gmail.com>
2022-10-06 22:58:23 +0100

    style

--- client/src/components/loaders/DetailsSkeleton.tsx
+++ client/src/components/loaders/DetailsSkeleton.tsx
@@ -6,7 +6,7 @@ const DetailsSkeleton = () => {
   return (
     <div>
       <SkeletonTheme baseColor="#d4d7d9" highlightColor="#e8eaeb">
-        <div className="grid grid-cols-8 gap-8 bg-white justify-between shadow-md p-4 mb-8">
+        <div className="lg:grid grid-cols-8 gap-8 bg-white justify-between shadow-md p-4 mb-8">
           <div className="col-span-4">
             <Skeleton className="h-96" />
           </div>
@@ -20,7 +20,7 @@ const DetailsSkeleton = () => {
             <Skeleton width={200} height={50} />
           </div>
         </div>
-        <div className=" shadow-md grid grid-cols-2 gap-8 mb-4">
+        <div className=" shadow-md lg:grid grid-cols-2 gap-8 mb-4">
           <div className="bg-white shadow p-2">
             <Skeleton height={100} />
           </div>
