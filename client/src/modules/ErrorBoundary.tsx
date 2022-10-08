0859aa86e8a62bee24dfa83365b35cedc6729359
ZinoChan <devzinochan@gmail.com>
2022-10-08 16:24:19 +0100

    refactor

--- client/src/modules/ErrorBoundary.tsx
+++ client/src/modules/ErrorBoundary.tsx
@@ -1,5 +1,5 @@
-import React, { Component, ErrorInfo, ReactNode } from 'react';
-
+import { Component, ErrorInfo, ReactNode } from 'react';
+import errorImg from '@/assets/error.svg';
 interface Props {
   children: ReactNode;
 }
@@ -24,7 +24,18 @@ class ErrorBoundary extends Component<Props, State> {
 
   public render() {
     if (this.state.hasError) {
-      return <h1>Sorry.. there was an error</h1>;
+      return (
+        <section className="py-6 bg-white min-h-screen flex items-center justify-center">
+          <div className="flex flex-col text-center space-y-4">
+            <img className="w-96 h-auto" src={errorImg} alt="empty order" />
+            <p className="text-secondary text-3xl font-bold text-gray-700">
+              Woops! <br /> Something went wrong.
+            </p>
+
+            <span className="text-gray-600 items-center">Have you tried turning it on and off again ?.</span>
+          </div>
+        </section>
+      );
     }
 
     return this.props.children;
