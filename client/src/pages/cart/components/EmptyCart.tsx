3bdaca1adefcf39ffd9d98ba6d6dfd7ae7d0dc90
ZinoChan <devzinochan@gmail.com>
2022-10-10 21:36:40 +0100

    minor change

--- client/src/pages/cart/components/EmptyCart.tsx
+++ client/src/pages/cart/components/EmptyCart.tsx
@@ -8,7 +8,7 @@ const EmptyCart = () => {
     <div className="flex items-center justify-center ">
       <div className="bg-white shadow-md text-center max-w-md p-4">
         <img src={emptyCart} alt="ampty cart" className="mb-2" />
-        <h3 className="text-primaryDark mb-2 font-bold text-2xl">Your cart is empty</h3>
+        <h3 className="text-primaryDark mb-2 font-bold text-2xl">Your Bag is empty</h3>
         <Button theme="btn-primary">
           <Link to={HOME}>Start shopping</Link>
         </Button>
