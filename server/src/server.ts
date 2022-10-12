80d565cbb88ea2fc3c419523bc963d15b8f4aec8
ZinoChan <devzinochan@gmail.com>
2022-10-12 11:15:21 +0100

    trying another fix to 404 on refresh

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,19 @@
+import path from 'path';
 import App from './app';
-import IndexRoute from './components';
 import CategoryRoute from './components/categories/categoryRoute';
 import OrderRoute from './components/orders/orderRoute';
 import ProductsRoute from './components/products/productRoute';
 import ReviewRoute from './components/reviews/reviewRoute';
 import UserRoute from './components/users/userRoute';
 
-const app = new App([
-  new ProductsRoute(),
-  new UserRoute(),
-  new ReviewRoute(),
-  new OrderRoute(),
-  new CategoryRoute(),
-  new IndexRoute(),
-]);
+const app = new App([new ProductsRoute(), new UserRoute(), new ReviewRoute(), new OrderRoute(), new CategoryRoute()]);
+
+if (process.env.NODE_ENV === 'production') {
+  app.getServer().get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../dist/index.html'));
+  });
+}
+
 app.listen();
 
 process.on('unhandledRejection', (err: NodeJS.ErrnoException) => {
