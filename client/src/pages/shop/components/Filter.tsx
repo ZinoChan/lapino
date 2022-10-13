cc8f8b18670ff409338f789984fad7fc38d351a5
ZinoChan <devzinochan@gmail.com>
2022-10-13 18:47:01 +0100

    refactor

--- client/src/pages/shop/components/Filter.tsx
+++ client/src/pages/shop/components/Filter.tsx
@@ -1,13 +1,35 @@
 import PriceRange from './PriceRange';
 import DiscountRange from './DiscountRange';
 import RatingRange from './RatingRange';
+import { useEffect, useState } from 'react';
+import { IProductRes } from '@/types/types';
 
-const Filter = ({searchQuery}: {searchQuery: string}) => (
+type Props = { setfilteredProducts: (filteredProducts: IProductRes[]) => void; products: IProductRes[] };
+
+const Filter = ({ products, setfilteredProducts }: Props) => {
+  const [ratingRange, setRatingRange] = useState(0);
+  const [pricingRange, setPricingRange] = useState([0, 200000]);
+  const [discountRange, setDiscountRange] = useState(0);
+
+  useEffect(() => {
+    setfilteredProducts(
+      products.filter(
+        (product) =>
+          product.pricing.originalPrice >= pricingRange[0] &&
+          product.pricing.originalPrice <= pricingRange[1] &&
+          product.rating >= ratingRange &&
+          product.pricing.discountPercentage >= discountRange,
+      ),
+    );
+  }, [pricingRange, ratingRange, discountRange]);
+
+  return (
     <>
-    <PriceRange  searchQuery={searchQuery}/>
-    <DiscountRange searchQuery={searchQuery} />
-    <RatingRange searchQuery={searchQuery} />
+      <PriceRange setPricingRange={setPricingRange} />
+      <DiscountRange setDiscountRange={setDiscountRange} />
+      <RatingRange setRatingRange={setRatingRange} />
     </>
-)
+  );
+};
 
-export default Filter
+export default Filter;
