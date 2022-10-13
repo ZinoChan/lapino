86a3eac9e0414df00ba87be69c023d9a6c309b7f
ZinoChan <devzinochan@gmail.com>
2022-10-13 18:46:16 +0100

    refactor rating filter

--- client/src/pages/shop/components/RatingRange.tsx
+++ client/src/pages/shop/components/RatingRange.tsx
@@ -1,23 +1,14 @@
 import { RatingView } from 'react-simple-star-rating';
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { SHOP } from '@/utils/routes';
 
-
-const RatingRange = ({searchQuery}: {searchQuery: string}) => {
+const RatingRange = ({ setRatingRange }: { setRatingRange: (rating: number) => void }) => {
   const ratingValues = [4, 3, 2, 1];
-  const [selectedRate, setSelectedRate] = useState(0)
-  const navigate = useNavigate()
-  
+  const [selectedRate, setSelectedRate] = useState(0);
+
   const onRateChange = (value: number) => {
-      setSelectedRate(value)
-      const ratingFilter = `rating[gte]=${value}`
-      if(searchQuery){
-        navigate(`${SHOP}${searchQuery}&&${ratingFilter}`)
-      }else{
-        navigate(`${SHOP}?${ratingFilter}`)
-      }
-  }
+    setSelectedRate(value);
+    setRatingRange(value);
+  };
 
   return (
     <div className="mb-10">
@@ -25,7 +16,13 @@ const RatingRange = ({searchQuery}: {searchQuery: string}) => {
       <div className="flex flex-col space-y-4">
         {ratingValues.map((value, index) => (
           <div key={value} className="flex items-center space-x-4">
-            <input value={value} onChange={() => onRateChange(value)} type="radio" checked={selectedRate === value} className="w-4 h-4" />
+            <input
+              value={value}
+              onChange={() => onRateChange(value)}
+              type="radio"
+              checked={selectedRate === value}
+              className="w-4 h-4"
+            />
             <div>
               <RatingView fillColor={'#fed900'} ratingValue={value} />
             </div>
