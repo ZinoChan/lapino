f78f8a973b992bf81d69b6b32087ecbeb3a36238
ZinoChan <devzinochan@gmail.com>
2022-10-31 21:30:27 +0100

    fix: remove redirection after admin logout

--- client/src/router/WithAdmin.tsx
+++ client/src/router/WithAdmin.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from 'react';
-import { useLocation, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useAppSelector } from '@/app/store';
 
 interface Props {
@@ -7,10 +7,9 @@ interface Props {
 }
 
 function WithAdmin({ children }: Props): ReactElement {
-  const location = useLocation();
   const auth = useAppSelector((state) => state.auth?.id && state.auth?.role === 'admin');
 
-  if (!auth) return <Navigate to="/login" state={{ from: location }} />;
+  if (!auth) return <Navigate to="/login" />;
 
   return children;
 }
